41685edf345c94fa49dfb8eb936f64905425cc21
Albert Tseng <albert@chegg.com>
2015-02-04 18:13:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FindDrug::Application.config.secret_key_base = '708f34b37cac3d9e6cb161e432b6a8b8dce97ccc4e475f519467ef45e9455e575943827b26ea81ca85c31f56f04e03f0058b0beaf44db868d99ff9b1171bde70'
